f1bfb12f15525b35d22454001163fa0bc7358256
Jason Akatiff <jasona@a4d.com>
2025-12-13 13:37:55 -0800

    fix: set correct down_revision for migration

--- backend/alembic/versions/add_page_fields_brand_scraped_ads.py
+++ backend/alembic/versions/add_page_fields_brand_scraped_ads.py
@@ -1,7 +1,7 @@
 """add page_name and page_link to brand_scraped_ads
 
 Revision ID: add_page_fields_001
-Revises:
+Revises: a1b2c3d4e5f6
 Create Date: 2025-12-13
 
 """
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = 'add_page_fields_001'
-down_revision = None
+down_revision = 'a1b2c3d4e5f6'
 branch_labels = None
 depends_on = None
 
